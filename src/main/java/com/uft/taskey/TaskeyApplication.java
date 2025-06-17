9ec9d86c31ac717b5862c292a80b1a8667f49de7
Yuri <119751708+comatic0@users.noreply.github.com>
2025-06-17 15:57:16 -0300

    initial commit

---
+++ src/main/java/com/uft/taskey/TaskeyApplication.java
@@ -0,0 +1,13 @@
+package com.uft.taskey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uft/taskey/TaskeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uft.taskey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
